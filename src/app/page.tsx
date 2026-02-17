bd6c83e3cba42013ff414bf2ba1fb4dfe59a33dc
CallensJ <callens.johan@protonmail.com>
2026-02-17 14:49:19 +0800

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,6 @@ import FinalCTA from "../components/homepage/FinalCTA";
 import HeroSection from "../components/homepage/HeroSection";
 import MiniGalerieSection from "../components/homepage/MiniSectionGallery";
 import UniversSection from "../components/homepage/UniversSection";
-import Navbar from "../components/layout/Navbar";
 
 export default function HomePage() {
   return (
